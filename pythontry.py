2bc8b896b94fa981dd246f8ddb1faff9780201fd
SHIVAMKUMAR51 <101336306+SHIVAMKUMAR51@users.noreply.github.com>
2022-04-10 01:29:56 +0530

    Add files via upload

---
+++ pythontry.py
@@ -0,0 +1,16 @@
+list1=input().split()
+l1 =[]
+for i in list1:
+    if int(i)>=0:
+        l1.append(int(i))
+
+print(l1)
+
+list2=input().split()
+l2=[]
+for i in list2:
+    if int(i)>=0:
+        l2.append(int(i))
+
+print(l2)        
+    
